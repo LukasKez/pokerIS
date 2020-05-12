1e391dc6269c96757f89e65afa92e50b1e54c456
Lukas Keževičius <l.kezevicius@gmail.com>
2020-05-12 13:04:57 +0300

    Initial

---
+++ WebApplication1/Controllers/GameController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class GameController : Controller
+    {
+        private readonly TableContext _context;
+
+        public GameController(TableContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Game
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Tables.ToListAsync());
+        }
+
+        // GET: Game/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var table = await _context.Tables
+                .SingleOrDefaultAsync(m => m.ID == id);
+            if (table == null)
+            {
+                return NotFound();
+            }
+
+            return View(table);
+        }
+
+        // GET: Game/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Game/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Seats,StartingBet")] Table table)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(table);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(table);
+        }
+
+        // GET: Game/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var table = await _context.Tables.SingleOrDefaultAsync(m => m.ID == id);
+            if (table == null)
+            {
+                return NotFound();
+            }
+            return View(table);
+        }
+
+        // POST: Game/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Seats,StartingBet")] Table table)
+        {
+            if (id != table.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(table);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TableExists(table.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(table);
+        }
+
+        // GET: Game/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var table = await _context.Tables
+                .SingleOrDefaultAsync(m => m.ID == id);
+            if (table == null)
+            {
+                return NotFound();
+            }
+
+            return View(table);
+        }
+
+        // POST: Game/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var table = await _context.Tables.SingleOrDefaultAsync(m => m.ID == id);
+            _context.Tables.Remove(table);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TableExists(int id)
+        {
+            return _context.Tables.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ WebApplication1/Data/DataInitializer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class DataInitializer
+    {
+        public static void Initialize(TableContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any students.
+            if (context.Tables.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            var tables = new Table[]
+            {
+                new Table{ID=0, Seats=8, StartingBet=50}
+            };
+
+            foreach (Table t in tables)
+            {
+                context.Tables.Add(t);
+            }
+            context.SaveChanges();
+        }
+    }
+}
