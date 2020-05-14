bec0e4b84f8f64c481f7cbf56ed7c6e43bc8b519
Lukas Keževičius <l.kezevicius@gmail.com>
2020-05-14 23:09:52 +0300

    updated to .NET Core 3.1

---
+++ Data/PokerISContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PokerIS.Models;
+
+namespace PokerIS.Data
+{
+    public class PokerISContext : DbContext
+    {
+        
+        public PokerISContext(DbContextOptions<PokerISContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Table> Table { get; set; }
+    }
+}
---
+++ Models/Table.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace PokerIS.Models
+{
+    public class Table
+    {
+        public int ID { get; set; }
+        public int Seats { get; set; }
+        public double StartingBet { get; set; }
+        [DefaultValue(0)]
+        public int PlayerCount { get; set; }
+    }
+}
